50c382f026282a66b6c7b9eff7d5b742716b624f
Ada Dyś <ada.dys@espeo.eu>
2017-03-17 11:13:08 +0100

    Access nested data with props.children

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            txt: "this is state text",
-            cat: "0"
-        }
-    }
-    update( e ) {
-        this.setState({txt: e.target.value})
-    }
     render(){
-        return (
-            <div>
-                <h1>Hello World</h1>
-                <Widget update={this.update.bind(this)}/>
-                <Widget update={this.update.bind(this)}/>
-                <p>{this.state.txt} - {this.state.cat}</p>
-            </div>
-        )
+        return <Button>I <Heart/> React</Button>
     }
 }
 
-const Widget = (props) =>
-    <input type="text" onChange={props.update} />
+const Button = (props) => <button>{props.children}</button>
 
-//const App = () => <h1>Hello stateless</h1>
-
-// The primary difference between a class component and a stateless component is thas class component can have STATE
-
-App.propTypes = {
-    txt: React.PropTypes.string,
-}
-
-App.defaultProps = {
-    txt: "default text"
+class Heart extends React.Component {
+    render(){
+        return <span>&hearts;</span>
+    }
 }
 
 export default App
