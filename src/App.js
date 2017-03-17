63b3f4a71f728e7b7d11320794fb9f757eb71137
Ada Dyś <ada.dys@espeo.eu>
2017-03-17 10:21:32 +0100

    Create class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const App = () => <h1>Hi</h1>
+class App extends React.Component {
+    render(){
+        return <h1>Hello World</h1>
+    }
+}
+
+//const App = () => <h1>Hello stateless</h1>
+
+// The primary difference between a class component and a stateless component is thas class component can have STATE
 
 export default App
